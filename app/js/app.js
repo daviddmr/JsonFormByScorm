3db16f288c8484d60db4c42658d5d7d61eb29fab
= <=>
2016-12-21 17:09:09 -0300

    Correção de importação das libs no index.html e no app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,14 @@
 angular.module("jsonForm", ["ngRoute", "ngMaterial", "ngMessages", "ngAnimate"])
     .config(['$routeProvider', "$mdThemingProvider",
-    function($routeProvider, $mdThemingProvider) {
-        $routeProvider
-            .when('/form', {
-                templateUrl: 'views/form.html',
-                controller: 'formCtrl'
-            })
-        .otherwise('/form');
+        function($routeProvider, $mdThemingProvider) {
+            $routeProvider
+                .when('/form', {
+                    templateUrl: 'app/views/form.html'
+                })
+                .otherwise('/form');
 
-         $mdThemingProvider.theme('default')
-            .primaryPalette('green')
-            .accentPalette('grey');
-    }])
+            $mdThemingProvider.theme('default')
+                .primaryPalette('green')
+                .accentPalette('grey');
+        }])
 ;
